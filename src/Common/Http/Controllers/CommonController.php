af39832b17adb81e24b550645f0038873104e528
Bob Bloom <bob.bloom@lasallesoftware.ca>
2020-06-02 13:32:35 -0400

    Move front-end classes over from the original library package. #2

---
+++ src/Common/Http/Controllers/CommonController.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * This file is part of the Lasalle Software library front-end package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * @copyright  (c) 2019-2020 The South LaSalle Trading Corporation
+ * @license    http://opensource.org/licenses/MIT
+ * @author     Bob Bloom
+ * @email      bob.bloom@lasallesoftware.ca
+ *
+ * @see       https://lasallesoftware.ca
+ * @see       https://packagist.org/packages/lasallesoftware/ls-libraryfrontend-pkg
+ * @see       https://github.com/LaSalleSoftware/ls-libraryfrontend-pkg
+ */
+
+namespace Lasallesoftware\Libraryfrontend\Common\Http\Controllers;
+
+// Laravel Framework
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
+
+class CommonController extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+}
