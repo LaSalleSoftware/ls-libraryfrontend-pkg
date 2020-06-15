f3effb4000fcdc6aec2e73ffed68fbcb163ced08
Bob Bloom <bob.bloom@lasallesoftware.ca>
2020-06-15 10:29:21 -0400

    Add the existing front-end app's config settings to this pkg's config file. #5

--- config/lasallesoftware-libraryfrontend.php
+++ config/lasallesoftware-libraryfrontend.php
@@ -60,6 +60,19 @@ return [
     */
     'lasalle_admin_api_url' => env('LASALLE_ADMIN_API_URL'),
 
+    /*
+    |--------------------------------------------------------------------------
+    | The key this front-end application uses for JWT
+    |--------------------------------------------------------------------------
+    |
+    | JWT is encrypted using this key.
+    |
+    | The administrative backend app must have this key to decrypt the JWT.
+    |
+    | Set in the .env file.
+    |
+    */
+    'lasalle_jwt_key' => env('LASALLE_JWT_KEY'),
 
     /*
     |--------------------------------------------------------------------------
@@ -86,58 +99,122 @@ return [
     | https://tools.ietf.org/html/rfc7519#section-4.1.4
 	|
 	*/
-    'lasalle_jwt_exp_claim_seconds_to_expiration' => 3600,    
+    'lasalle_jwt_exp_claim_seconds_to_expiration' => 3600,
+
+    /*
+	|--------------------------------------------------------------------------
+	| Date Format
+	|--------------------------------------------------------------------------
+	|
+    | What date format do you want for your front-end views?
+    |
+	| https://www.php.net/manual/en/function.date.php
+	|
+	*/
+    'lasalle_date_format'  => 'F d, Y',
+
+    /*
+	|--------------------------------------------------------------------------
+	| Pagination: How Many Items To Display "Per Page"
+	|--------------------------------------------------------------------------
+	|
+	| When displaying a list of items, such as a list of blog posts, how many items do you want
+	| to display? For example, if there are 100 blog posts, do you want to display all 100 posts? Probably
+	| not. Instead, you probably want to display, say, 15 blog posts at a time. In which case, set this
+    | config parameter to "15"
+    |
+    | https://laravel.com/docs/6.x/pagination
+    |
+	*/
+    //'lasalle_pagination_number_of_items_displayed_per_page'  => 'none',
+    'lasalle_pagination_number_of_items_displayed_per_page'  => 6,
+
+    /*
+	|--------------------------------------------------------------------------
+	| Front-end blade view folder's path
+	|--------------------------------------------------------------------------
+	|
+	| Where is the blade folder that you are using?
+	|
+    | The LaSalle UI package's "base" folder is the default.
+	|
+	*/
+    'lasalle_path_to_front_end_view_path' => 'lasallesoftwarelasalleuifrontend::base',
+
+
+    /*
+	|--------------------------------------------------------------------------
+	| Default Featured Image
+	|--------------------------------------------------------------------------
+	|
+	| When no featured image is specified, use this image.
+	|
+	| Specify the type of image by commenting out the types that do not apply.
+    |  * "external_file" = what you want in the "src" of the IMG html tag (<img src="" >)
+    |                      such as a full URL where the image resides, or base64 code
+    |  * "code" = the entire html code encompassing the image, that will be rendered in html literally
+	|
+	*/
+    'lasalle_featured_image_default_type'  => 'external_file',
+    //'lasalle_featured_image_default_type'  => 'code',
+    'lasalle_featured_image_default_image' => '/nature1.jpg',
+
+    /*
+	|--------------------------------------------------------------------------
+	| Number of Recent Blog Posts to Display on the Home Page
+	|--------------------------------------------------------------------------
+	|
+	| How many recent blog posts do you want to display on the home page? 
+	|
+	| If you select "0" (zero), then perhaps you should bypass the home page controller.
+	|
+	*/
+    'lasalle_number_of_recent_blog_posts_to_display_on_the_home_page'  => 5,
+
+    /*
+	|--------------------------------------------------------------------------
+	| Social Media Meta Tag: Twitter Handle for the "twitter:site" tag
+	|--------------------------------------------------------------------------
+	|
+    | This is a Twitter handle, such as '@bobbloom'.
+    |
+	| For the twitter:site meta tag
+	|
+	*/
+    'lasalle_social_media_meta_tag_site'  => '@bobbloom',
+
+    /*
+	|--------------------------------------------------------------------------
+	| Social Media Meta Tag: Twitter Handle for the "twitter:creator" tag
+	|--------------------------------------------------------------------------
+	|
+    | This is a Twitter handle, such as '@bobbloom'.
+    |
+	| For the twitter:creator meta tag.
+	|
+	*/
+    'lasalle_social_media_meta_tag_creator'  => '@bobbloom',
+
+    /*
+	|--------------------------------------------------------------------------
+	| Default Social Media Tag Image
+	|--------------------------------------------------------------------------
+	|
+	| When no social media image is specified, use this image.
+	|
+	| Specify the full URL!
+	|
+	| A social media tag is a Twitter card meta tag, or an Open Graph meta card, or something of that nature.
+    |
+	| According to https://developer.twitter.com/en/docs/tweets/optimize-with-cards/overview/markup,
+	| "URL of image to use in the card. Images must be less than 5MB in size. JPG, PNG, WEBP and GIF
+	| formats are supported. Only the first frame of an animated GIF will be used. SVG is not supported."
+	|
+	*/
+	'lasalle_social_media_meta_tag_default_image'  => 'https://lasallesoftware.ca/nature1.jpg',
 
     
     
-    /*
-   | ========================================================================
-   | START: PATHS FOR FEATURED IMAGES
-   | ========================================================================
-   |
-   | You may want to store featured image images (!) in S3 folders, and not just
-   | in S3 buckets. And, you may want store Nova resource featured images in
-   | their own folders. You can specify individual S3 folders here for profile
-   | and blog resources.
-   |
-   | Must have a leading slash.
-   | Must not have a trailing slash.
-   |
-   | Do not want to use an S3 folder at all? Just put the images in the S3 bucket?
-   | Then, just specify '/',
-   |
-   | I designed this specifically for S3, but it applies generally because Nova
-   | uses Laravel's storage facade
-   | * https://laravel.com/docs/master/filesystem
-   | * https://nova.laravel.com/docs/2.0/resources/file-fields.html#file-fields
-   |
-   | IMPORTANT!!! ****You need to set up each S3 folder in your AWS console.****
-   | See https://github.com/LaSalleSoftware/ls-adminbackend-app/blob/master/AWS_S3_NOTES_README.md
-   |
-   */
-
-    // for Nova resources in the novabackend package
-    'image_path_for_address_nova_resource' => '/',
-    //'image_path_for_address_nova_resource' => '/address',
-
-    'image_path_for_company_nova_resource' => '/',
-    //'image_path_for_company_nova_resource' => '/company',
-
-    'image_path_for_person_nova_resource'  => '/',
-    //'image_path_for_person_nova_resource'  => '/person',
-
-
-    // for Nova resources in the blogbackend package
-    'image_path_for_category_nova_resource' => '/',
-    //'image_path_for_category_nova_resource' => '/category',
-
-    'image_path_for_post_nova_resource'     => '/',
-    //'image_path_for_post_nova_resource'     => '/post',
-
-    /*
-   | ========================================================================
-   | END: PATHS FOR FEATURED IMAGES
-   | ========================================================================
-   */
+   
 
 ];
